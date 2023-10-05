927ac1a01a446381325b58fff89afe41e68a1f91
Ana Cristina Azevedo <105959923+anacristinaazevedo@users.noreply.github.com>
2023-10-05 10:53:54 -0300

    Add files via upload

---
+++ maior numero sorteadocop.js
@@ -0,0 +1,20 @@
+
+const { gets, print }  = require('./aula7cop.js');
+
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    numerosSorteados.push(numeroSorteado);
+}
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numeroSorteado = numerosSorteados[i];
+    if (numeroSorteado > maiorValor){
+        maiorValor = numeroSorteado;
+    }
+};
+
+print(numerosSorteados);
